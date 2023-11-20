18ce3dcc955d43946a8aac838a3170b247049e72
Lussankya <leducthanh45@gmail.com>
2023-11-20 17:51:39 +0700

    init

---
+++ app/src/main/java/com/mad/group8/snake/acitivity/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.mad.group8.snake.acitivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.mad.group8.snake.R;
+
+public class HomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
